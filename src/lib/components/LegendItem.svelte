4711855101184c76bc78cb84b152de93a8caedbd
Kenned Ferreira <74821126+kennedfer@users.noreply.github.com>
2025-07-24 18:15:46 +0000

    feat: Reorganize the code structure and change delete icon to trash icon

--- src/lib/components/LegendItem.svelte
+++ src/lib/components/LegendItem.svelte
@@ -15,27 +15,27 @@
 	const updateItem = (item: LegendItem) => legendStore.updateItem(item);
 </script>
 
-<li
-	class:selected={$selectedItem?.id === item.id}
-	on:click={() => selectedItem.set(item)}
-	class={`m-1 flex h-10 cursor-pointer justify-between gap-2 rounded-sm p-1 transition-all duration-300 ease-in-out ${$selectedItem?.id === item.id ? 'bg-amber-200' : 'bg-none hover:bg-amber-100'}`}
->
-	<div class="inline-flex w-full items-center gap-2">
-		<input
-			class="h-10 w-9 appearance-none rounded-lg border-none outline-none"
-			id={`item-color-${item.id}`}
-			type="color"
-			bind:value={item.color}
-			on:change={() => updateItem(item)}
-		/>
-		<input id={`item-name-${item.id}`} type="text" bind:value={item.name} on:change={() => updateItem(item)} placeholder="Name..." minlength="1" />
-	</div>
-	<div class="inline-flex items-center gap-1">
-		<button
-			class="aspect-square cursor-pointer rounded-sm text-red-700 transition-all duration-300 ease-in-out hover:bg-red-700 hover:text-white"
-			on:click={() => deleteItem(item)}
-		>
-			<i class="fa-solid fa-x w-7"></i>
-		</button>
-	</div>
+<li class={`flex box-border h-11 cursor-pointer justify-between gap-2 rounded-sm p-1`}>
+	<input
+		class="h-full w-8 aspect-square appearance-none rounded-lg border-none outline-none"
+		id={`item-color-${item.id}`}
+		type="color"
+		bind:value={item.color}
+		on:change={() => updateItem(item)}
+	/>
+	<input
+		class="w-full rounded-sm p-1 outline-gray-300 focus:outline-1"
+		id={`item-name-${item.id}`}
+		type="text"
+		bind:value={item.name}
+		on:change={() => updateItem(item)}
+		placeholder="Name..."
+		minlength="1"
+	/>
+	<button
+		aria-label="delete legend item"
+		class="aspect-square cursor-pointer rounded-sm text-red-400 transition-all duration-300 ease-in-out hover:bg-red-400 hover:text-white"
+		on:click={() => deleteItem(item)}
+	><i class="fa-solid fa-trash w-7"></i>
+	</button>
 </li>
