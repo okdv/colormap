98a3517022ebbf46b03e8300bec7441b1262b853
Kenned Ferreira <74821126+kennedfer@users.noreply.github.com>
2025-07-24 18:52:55 +0000

    feat: Add a new style to control pannel and toolbar

--- src/lib/components/Legend.svelte
+++ src/lib/components/Legend.svelte
@@ -4,7 +4,7 @@
 	import { LegendItem } from '$lib/components';
 </script>
 
-<ul class="w-96 list-none p-2">
+<ul class="w-96 list-none p-2 gap-2 flex flex-col">
 	<!-- convert Mapped Records to array of values, each being a legend item -->
 	{#each Object.values($legendStore) as item}
 		<LegendItem {item} />
