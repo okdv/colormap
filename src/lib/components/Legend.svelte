af48a04a558ee55fb1e1396684469ac0ef8f38c0
okdv <dev@otho.dev>
2025-07-26 05:05:24 +0000

    fix: add key to legend foreach

--- src/lib/components/Legend.svelte
+++ src/lib/components/Legend.svelte
@@ -6,7 +6,7 @@
 
 <ul class="flex w-96 list-none flex-col gap-2 p-2">
 	<!-- convert Mapped Records to array of values, each being a legend item -->
-	{#each Object.values($legendStore) as item}
+	{#each Object.values($legendStore) as item (item.id)}
 		<LegendItem {item} />
 	{/each}
 </ul>
