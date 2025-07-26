f8ff36d90db1a40faac57601d43b5a9cfcdfe78a
okdv <dev@otho.dev>
2025-07-26 04:31:07 +0000

    feat: support geojson selection via url param

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,2 +1,16 @@
+import { selectedLayerStore } from "$lib/stores";
+import type { PageLoad } from "./$types";
+
 export const ssr = false;
 export const prerender = true;
+
+// support selecting geojson via URL type param
+export const load: PageLoad = ({ url }) => {
+    // get geojson from URL params
+    const layerName = url.searchParams.get('type');
+
+    // if url param is present, use it
+    if (layerName && layerName.length > 0) {
+        selectedLayerStore.selectLayer(layerName);
+    }
+}
