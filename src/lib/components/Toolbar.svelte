43dcb95fc71db152f2d889f97a908285d845f838
okdv <dev@otho.dev>
2025-07-26 20:29:50 +0000

    feat: trigger settings modal from toolbar

--- src/lib/components/Toolbar.svelte
+++ src/lib/components/Toolbar.svelte
@@ -1,8 +1,11 @@
-<script>
+<script lang="ts">
 	// src/lib/components/Toolbar.svelte
 
 	import { legendStore } from '$lib/stores';
 	import { LegendItem } from '$lib/types';
+	import Modal from './Modal.svelte';
+
+	let settingsModal: boolean = false 
 </script>
 
 <div class="rounded-t-md border-b border-gray-300 bg-gray-100 p-2">
@@ -14,6 +17,15 @@
 		<span>New</span>
 	</button>
 	<button
+		class="cursor-pointer rounded-sm p-1 px-2 text-blue-600 transition-all duration-300 ease-in-out hover:bg-blue-400 hover:text-white"
+		on:click={() => {
+			settingsModal = true;
+		}}
+	>
+		<i class="fa-solid fa-cog"></i>
+		<span>Settings</span>
+	</button>
+	<button
 		class="cursor-pointer rounded-sm p-1 px-2 text-red-400 transition-all duration-300 ease-in-out hover:bg-red-400 hover:text-white"
 		on:click={() => {
 			legendStore.clearItems();
@@ -23,3 +35,9 @@
 		<span>Clear</span>
 	</button>
 </div>
+
+{#if settingsModal}
+	<Modal bind:open={settingsModal}>
+		<div>Settings</div>
+	</Modal>
+{/if}
