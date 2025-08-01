c57eb83b8e9811eeb0e3a03bce06f8afaa9a46c5
okdv <dev@otho.dev>
2025-08-01 18:59:20 +0000

    chore: use baseUrl in playwright

--- tests/map-load.test.ts
+++ tests/map-load.test.ts
@@ -3,7 +3,7 @@ import { expect, test } from '@playwright/test';
 // may throw errors running locally due to browser dependencies, i think it should work in github actions though
 
 test('should load the webapp and leaflet map', async ({ page }) => {
-	await page.goto('http://localhost:4173');
+	await page.goto('/');
 	// confirm map container is visible
 	await expect(page.locator('#map-container')).toBeVisible();
 	// check that map tiles are rendering by looking for attribution (not perfect but ok for now)
