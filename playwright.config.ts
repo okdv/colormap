eca863e2ff1b05ce64ad286ac25e06ca51ecb057
okdv <dev@otho.dev>
2025-08-01 18:57:05 +0000

    fix: re-init playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,23 +1,38 @@
 import { defineConfig, devices } from '@playwright/test';
 
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// import dotenv from 'dotenv';
+// import path from 'path';
+// dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
 export default defineConfig({
-	webServer: {
-		command: 'npm run build && npm run preview',
-		port: 4173,
-		reuseExistingServer: !process.env.CI
-	},
-	testDir: './e2e',
-	fullyParallel: true, // run tests in parallel
-	retries: process.env.CI ? 2 : 0, // retry tests if in github action
-	workers: process.env.CI ? 1 : undefined, // run with one worker only in github action
-	forbidOnly: !!process.env.CI, // fail in github action if left test.only in code anywhere
-	reporter: 'html', // https://playwright.dev/docs/test-reporters
-	// shared settings for all projects
+	testDir: './tests',
+	/* Run tests in files in parallel */
+	fullyParallel: true,
+	/* Fail the build on CI if you accidentally left test.only in the source code. */
+	forbidOnly: !!process.env.CI,
+	/* Retry on CI only */
+	retries: process.env.CI ? 2 : 0,
+	/* Opt out of parallel tests on CI. */
+	workers: process.env.CI ? 1 : undefined,
+	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
+	reporter: 'html',
+	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 	use: {
-		baseURL: 'http://localhost:4173',
+		/* Base URL to use in actions like `await page.goto('/')`. */
+		// baseURL: 'http://localhost:3000',
+
+		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
 		trace: 'on-first-retry'
 	},
-	// projects for major browsers/environments
+
+	/* Configure projects for major browsers */
 	projects: [
 		{
 			name: 'chromium',
@@ -33,6 +48,7 @@ export default defineConfig({
 			name: 'webkit',
 			use: { ...devices['Desktop Safari'] }
 		}
+
 		/* Test against mobile viewports. */
 		// {
 		//   name: 'Mobile Chrome',
@@ -53,4 +69,11 @@ export default defineConfig({
 		//   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
 		// },
 	]
+
+	/* Run your local dev server before starting the tests */
+	// webServer: {
+	//   command: 'npm run start',
+	//   url: 'http://localhost:3000',
+	//   reuseExistingServer: !process.env.CI,
+	// },
 });
