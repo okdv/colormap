5c4496812d3c921563d6d691491c2cd57b282b40
okdv <dev@otho.dev>
2025-08-08 17:37:35 +0000

    chore: move existing Footer to ControlPanelFooter

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,6 +1,7 @@
 // src/lib/components/index.ts
 
 export { default as ControlPanel } from './ControlPanel.svelte';
+export { default as ControlPanelFooter } from './ControlPanelFooter.svelte';
 export { default as Legend } from './Legend.svelte';
 export { default as LegendItem } from './LegendItem.svelte';
 export { default as Map } from './Map.svelte';
