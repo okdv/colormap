6edcc6bff0a6d7ec0136fd69ecf5073448832f89
okdv <dev@otho.dev>
2025-08-10 05:52:14 +0000

    chore: cleanup dropdown

--- src/lib/components/Dropdown.svelte
+++ src/lib/components/Dropdown.svelte
@@ -1,12 +1,12 @@
 <script lang="ts"></script>
 <div class="group relative">
-    <a href="#">
+    <a href="#" class="transition duration-200 ease-in-out group-hover:text-yellow-400">
         <slot name="link" />
         <span>
-            <i class="fa-solid fa-angle-down fa-sm transform group-hover:rotate-180 transition-transform duration-200"></i>
+            <i class="fa-solid fa-angle-down fa-sm transform group-hover:rotate-180 transition-transform duration-200 ease-in-out"></i>
         </span>
     </a>
-    <div class="hidden group-hover:block absolute top-full bg-zinc-50 -translate-x-1/2 w-48 overflow-hidden p-2 z-1000 text-zinc-950 rounded-md shadow-2xl">
+    <div class="hidden group-hover:block absolute top-full bg-zinc-50 -translate-x-1/2 w-48 overflow-hidden p-2 z-1000 ease-in-out text-zinc-950 rounded-md shadow-2xl">
         <slot name="body" />
     </div>
 </div>
