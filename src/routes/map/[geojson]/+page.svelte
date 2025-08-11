8848f779f9296c42ad331177fb33330b3fd8a055
okdv <dev@otho.dev>
2025-08-11 00:09:08 +0000

    chore: add map route

---
+++ src/routes/map/[geojson]/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { Map } from '$lib/components';
+    export let data; 
+</script>
+
+<!-- height = screenheight minus heigh of navbar (4rem / h-16)-->
+<section class="h-[calc(100vh-4rem)]">
+	<Map />
+</section>
