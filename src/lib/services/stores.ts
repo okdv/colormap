c80ebb0f6ba7b1dcd0fe72ca820a9f41f93c9e19
okdv <dev@otho.dev>
2025-08-14 21:30:31 +0000

    chore: pre-push

--- src/lib/services/stores.ts
+++ src/lib/services/stores.ts
@@ -61,34 +61,34 @@ export const getDeepClonedValue = <T>(store: Writable<T>) => {
  */
 const persistToLocalStorage = <T>(key: string, value: T) => {
 	try {
-		console.log(key + ' saved to localSTorage')
+		console.log(key + ' saved to localSTorage');
 		localStorage.setItem(key, JSON.stringify(value));
-	} catch(err) {
-		console.error(`Error saving ${key} to localStorage: ${err}`)
+	} catch (err) {
+		console.error(`Error saving ${key} to localStorage: ${err}`);
 	}
-}
+};
 
 /**
  * Executes a function with a queueing delay, particularly used for creating a debounce behavior between svelte store and localStorage to sort of 'queue' saves
- * @param func (Function) this is the function that will be executed on the debounce, can have any number or type of args 
+ * @param func (Function) this is the function that will be executed on the debounce, can have any number or type of args
  * @param delay (number) ms delay that is passed to setTimout
  */
 const debounce = <T extends any[]>(func: Func<T>, delay: number): Func<T> => {
 	let timeout: ReturnType<typeof setTimeout>;
-	console.log('debounced')
+	console.log('debounced');
 
-	return function(this: any, ...args: T) {
-		const context = this; 
+	return function (this: any, ...args: T) {
+		const context = this;
 		clearTimeout(timeout);
 		timeout = setTimeout(() => func.apply(context, args), delay);
-	}
-}
+	};
+};
 
 /**
  * Creates and syncs svelte store and localStorage with initial or existing values, or updated items from the store subscription
  * @param storeName the localstorage store specific key, e.g. key='fooBar' results in localStorage entry named "colormap.fooBar"
  * @param initValue initial value for the store and localstorage, usually a default value'
- * @param delay integer that sets a debounce delay for localStorage to be updated, defaults to 0, which bypasses debounce 
+ * @param delay integer that sets a debounce delay for localStorage to be updated, defaults to 0, which bypasses debounce
  * @returns a svelte store
  */
 export const storeData = <T>(storeName: string, initValue: T, delay: number = 0): Writable<T> => {
@@ -117,12 +117,12 @@ export const storeData = <T>(storeName: string, initValue: T, delay: number = 0)
 		if (delay > 0) {
 			const debouncedSaved = debounce(persistToLocalStorage, delay);
 			store.subscribe((value) => {
-				debouncedSaved(key, value)
-			})
+				debouncedSaved(key, value);
+			});
 		} else {
 			store.subscribe((value) => {
-				persistToLocalStorage(key, value)
-			})
+				persistToLocalStorage(key, value);
+			});
 		}
 	}
 
