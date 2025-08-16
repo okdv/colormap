af636ce24b0dcca7db407e127ba4d601f28af4ea
okdv <dev@otho.dev>
2025-08-16 06:41:47 +0000

    feat: move legendStore to new data structure and update functions

--- src/lib/stores/legend.ts
+++ src/lib/stores/legend.ts
@@ -1,6 +1,6 @@
-import type { LegendItem } from '$lib/types';
+import type { LegendLocalStorage, LegendItem, Legend } from '$lib/types';
 import { get, writable } from 'svelte/store';
-import { addRecordToStore, removeRecordFromStore, storeData, updateRecordInStore } from '$lib/services';
+import { addRecordToNestedStore, addRecordToStore, removeRecordFromNestedStore, removeRecordFromStore, storeData, updateRecordInNestedStore, updateRecordInStore } from '$lib/services';
 
 /**
  * Creates legend store and persists it to local storage (or uses an existing local storage if available)
@@ -14,15 +14,22 @@ const createLegend = () => {
 		color: '#ff0000'
 	};
 	// default legend
-	const legend: Record<string, LegendItem> = {
+	const legendSet: Record<string, LegendItem> = {
 		[defaultItem.id]: defaultItem
 	};
 
 	// create store
-	const store = storeData<Record<string, LegendItem>>('legend', legend);
+	const store = storeData<LegendLocalStorage>('legend', {});
 
 	return {
 		subscribe: store.subscribe,
+		addMap: (mapId: string) => addRecordToStore<Legend>(mapId, legendSet, store),
+		removeMap: (mapId: string) => removeRecordFromStore<Legend>(mapId, store),
+		clearMap: (mapId: string) => updateRecordInStore<Legend>(mapId, legendSet, store),
+		addLegendItem: (mapId: string, item: LegendItem) => addRecordToNestedStore(mapId, item.id, item, store),
+		removeLegendItem: (mapId: string, itemId: string) => removeRecordFromNestedStore(mapId, itemId, store),
+		updateLegendItem: (mapId: string, newItem: LegendItem) => updateRecordInNestedStore(mapId, newItem.id, newItem, store),
+
 		updateItem: (newItem: LegendItem) => updateRecordInStore(newItem.id, newItem, store),
 		addItem: (newItem: LegendItem) => addRecordToStore(newItem.id, newItem, store),
 		removeItem: (id: string) => removeRecordFromStore(id, store),
