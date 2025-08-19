f407026a816de0ec5122f77d3fa48c565c127e75
okdv <dev@otho.dev>
2025-08-19 01:54:21 +0000

    chore: add e2e test

---
+++ tests/app-nav.test.ts
@@ -0,0 +1,18 @@
+import { getStaticFile } from '../src/lib/services/utils.server';
+import type { InteractiveLayer } from '$lib/types';
+import { test } from '@playwright/test';
+
+let manifest: InteractiveLayer[] = getStaticFile('manifest.json')
+
+test.describe('Prerendered Map Pages', () => {
+    if (!manifest || manifest.length === 0) {
+        test.fail('No manifest data available', () => {})
+        return;
+    }
+
+    for (const layer of manifest) {
+        test(`should correctly prerender the page for slug: ${layer.id}`, async ({ page }) => {
+            await page.goto(`/map/${layer.id}`)
+        })
+    }
+});
