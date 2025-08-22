a3f1df50f47d57f8757180b6feb845fd43843b9c
okdv <dev@otho.dev>
2025-08-22 03:56:48 +0000

    feat: make getStaticFile more reusable

--- src/lib/server/utils.server.ts
+++ src/lib/server/utils.server.ts
@@ -5,13 +5,15 @@ import { resolve } from 'path';
 import type { InteractiveLayer } from '$lib/types';
 
 /**
- * returns a file from the static/data directory using only filepath utilities
- * @returns parsed JSON of the files content
+ * returns a file using only filepath utilities
+ * @param filepath location of file, e.g. ./static/data/
+ * @param filename name of file, e.g. manifest.json
+ * @returns parsed file content as string
  */
-export const getStaticFile = (filename: string) => {
+export const getFile = (filepath: string, filename: string): string => {
 	const filePath = resolve('./static/data/', filename);
 	const fileContent = readFileSync(filePath, 'utf-8');
-	return JSON.parse(fileContent);
+	return fileContent;
 };
 
 // build time cache of interactive layers to avoid duplicate calls to retrieve manifest
@@ -21,5 +23,6 @@ export const getManifest = (): InteractiveLayer[] => {
 	if (interactiveLayers) {
 		return interactiveLayers;
 	}
-	return getStaticFile('manifest.json');
+	const manifestStr = getFile('./static/data/', 'manifest.json');
+	return JSON.parse(manifestStr)
 };
