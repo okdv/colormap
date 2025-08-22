f66fd260ee6d3859a5223af212c2614defef4f66
okdv <dev@otho.dev>
2025-08-22 23:52:41 +0000

    chore: remove button component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import { Button } from "$lib/components";
 import type { LayoutData } from "./$types";
 
 	// src/routes/+page.svelte
@@ -9,8 +8,8 @@ import type { LayoutData } from "./$types";
 
 <!-- height = screenheight minus heigh of navbar (4rem / h-16)-->
 <div class="h-[calc(100vh-4rem)]">
-	<header>
-		<div class="text-center">
+	<header class="p-2">
+		<div class="text-center space-y-2">
 			<h1 class="text-3xl">Choose a Layer to Create Your Own Map</h1>
 			<p class="text-xl">Select features, like states and counties, on one of our interactive map layers to color them in per your own custom legend. Whether you're looking to graph political data or just visualize all those places you've roadtripped, you can get started in seconds. 100% free and open-source, no signups, no ads, no tracking, just a simple tool that runs right in your browser.</p>
 		</div>
@@ -18,9 +17,9 @@ import type { LayoutData } from "./$types";
 			{#each data.interactiveLayers as layer, i (i)}
 				<a href="/map/{layer.id}">
 					<li>
-						<Button handleClick={() => legendStore.clearMap(interactiveLayer.id)}>
+						<button on:click={() => legendStore.clearMap(interactiveLayer.id)} class="cursor-pointer rounded-sm p-1 px-2 text-red-400 transition-all duration-300 ease-in-out hover:bg-red-400 hover:text-white">
 							<h3 class="text-2xl">{layer.name}</h3>
-						</Button>
+						</button>
 					</li>
 				</a>
 			{/each}
