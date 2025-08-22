c887ad885ea5fb00a876fc5c38e179cef0e2b643
okdv <dev@otho.dev>
2025-08-22 01:21:01 +0000

    feat: add header

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,28 @@
 <script lang="ts">
+	import type { LayoutData } from "./$types";
+
 	// src/routes/+page.svelte
+
+	export let data: LayoutData;
 </script>
 
 <!-- height = screenheight minus heigh of navbar (4rem / h-16)-->
-<section class="h-[calc(100vh-4rem)]">
-	<h1>homepage</h1>
-</section>
+<div class="h-[calc(100vh-4rem)]">
+	<header>
+		<div class="text-center">
+			<h1 class="text-3xl">Choose a Layer to Create Your Own Map</h1>
+			<p class="text-xl">Select features, like states and counties, on one of our interactive map layers to color them in per your own custom legend. Whether you're looking to graph political data or just visualize all those places you've roadtripped, you can get started in seconds. 100% free and open-source, no signups, no ads, no tracking, just a simple tool that runs right in your browser.</p>
+		</div>
+		<ul class="flex justify-around">
+			{#each data.interactiveLayers as layer, i (i)}
+				<a href="/map/{layer.id}">
+					<li>
+						<h3 class="text-2xl">{layer.name}</h3>
+					</li>
+				</a>
+			{/each}
+		</ul>
+	</header>
+	<main></main>
+	<footer></footer>
+</div>
