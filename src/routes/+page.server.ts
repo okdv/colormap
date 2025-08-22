961b244c51c3d295e347004bf02906e1266bed36
okdv <dev@otho.dev>
2025-08-22 04:19:41 +0000

    fix: return formatted and trimmed html at build

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -7,16 +7,21 @@ export const ssr = false;
 export const prerender = true;
 
 const interactiveLayers: InteractiveLayer[] = getManifest();
-const readme = mdToHtml(getFile('./', 'README.md'))
-const contributing = mdToHtml(getFile('./', 'CONTRIBUTING.md'))
+const readmeMd = getFile('./', 'README.md')
+const contributingMd = getFile('./', 'CONTRIBUTING.md')
 
 export const load: PageServerLoad = async () => {
+	let readme: string;
+	let contributing: string;
 	if (!interactiveLayers || interactiveLayers.length === 0) {
 		throw error(500, 'Unable to locate manifest data');
 	}
-	if (!readme || !contributing || readme.length === 0 || contributing.length === 0) {
+	if (!readmeMd || !contributingMd || readmeMd.length === 0 || contributingMd.length === 0) {
 		throw error(500, "Unable to get markdown files")
 	}
+
+	readme = mdToHtml(readmeMd, true)
+	contributing = mdToHtml(contributingMd, true)
 
 	return {
 		interactiveLayers,
