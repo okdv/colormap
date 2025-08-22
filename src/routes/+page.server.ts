b2e6f2c57f67ea77b10a6c7c3e852d2a25fcd181
okdv <dev@otho.dev>
2025-08-22 03:42:13 +0000

    chore: update homepage SSR

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,9 +1,19 @@
-import type { PageLoad } from './$types';
+import { getManifest } from '$lib/server/utils.server';
+import type { InteractiveLayer } from '$lib/types';
+import { error } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
 
 export const ssr = false;
 export const prerender = true;
 
-// support selecting geojson via URL type param
-export const load: PageLoad = () => {
-	console.log('homepage loaded')
+const interactiveLayers: InteractiveLayer[] = getManifest();
+
+export const load: PageServerLoad = async () => {
+	if (!interactiveLayers || interactiveLayers.length === 0) {
+		throw error(500, 'Unable to locate manifest data');
+	}
+
+	return {
+		interactiveLayers
+	};
 };
