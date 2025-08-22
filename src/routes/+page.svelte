651308b1cede863555cd959516e927ab52f859aa
okdv <dev@otho.dev>
2025-08-22 23:55:55 +0000

    chore: enhance buttons on homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,11 +13,11 @@ import type { LayoutData } from "./$types";
 			<h1 class="text-3xl">Choose a Layer to Create Your Own Map</h1>
 			<p class="text-xl">Select features, like states and counties, on one of our interactive map layers to color them in per your own custom legend. Whether you're looking to graph political data or just visualize all those places you've roadtripped, you can get started in seconds. 100% free and open-source, no signups, no ads, no tracking, just a simple tool that runs right in your browser.</p>
 		</div>
-		<ul class="flex justify-around">
+		<ul class="flex justify-center gap-20 p-2">
 			{#each data.interactiveLayers as layer, i (i)}
 				<a href="/map/{layer.id}">
 					<li>
-						<button on:click={() => legendStore.clearMap(interactiveLayer.id)} class="cursor-pointer rounded-sm p-1 px-2 text-red-400 transition-all duration-300 ease-in-out hover:bg-red-400 hover:text-white">
+						<button on:click={() => legendStore.clearMap(interactiveLayer.id)} class="cursor-pointer shadow-xl font-semibold rounded-sm p-2 border-2 border-solid border-blue-700 text-blue-700 transition-all duration-300 ease-in-out hover:bg-blue-700 hover:text-white hover:shadow">
 							<h3 class="text-2xl">{layer.name}</h3>
 						</button>
 					</li>
