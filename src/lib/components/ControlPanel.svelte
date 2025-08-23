fe468ac26a2cff93e33ceddbc10a008f8376aa77
okdv <dev@otho.dev>
2025-08-23 22:59:56 +0000

    fix: remove control panel footer

--- src/lib/components/ControlPanel.svelte
+++ src/lib/components/ControlPanel.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	// src/lib/components/ControlPanel.svelte
-	import { Legend, Toolbar, ControlPanelFooter } from '$lib/components';
+	import { Legend, Toolbar } from '$lib/components';
 	import type { InteractiveLayer } from '$lib/types';
 
 	export let interactiveLayer: InteractiveLayer;
@@ -9,5 +9,4 @@
 <div class="absolute right-4 bottom-4 z-1000 rounded-md border-1 border-gray-300 bg-white shadow-lg">
 	<Toolbar {interactiveLayer} />
 	<Legend {interactiveLayer} />
-	<ControlPanelFooter />
 </div>
